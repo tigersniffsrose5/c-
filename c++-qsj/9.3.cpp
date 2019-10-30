20f39b26a6bad96d3987880af7cedb3853d25625
tigersniffsrose5 <1228335053@qq.com>
2019-10-30 21:19:58 +0800

    qsj

---
+++ c++-qsj/9.3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool find(vector<int>::iterator begin, vector<int>::iterator end, int value)
+{
+    for ( auto iter = begin; iter != end; ++iter )
+        if ( *iter == value )
+            return true;
+    return false;
+}
+
+vector<int>::iterator Find(vector<int>::iterator begin, vector<int>::iterator end, int value)
+{
+    for ( auto iter = begin; iter != end; ++iter )
+        if ( *iter == value )
+            return iter;
+    return end;
+}
