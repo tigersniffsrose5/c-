f3d045a212cfb79a410f92184167d99f7bceb880
tigersniffsrose5 <1228335053@qq.com>
2019-11-20 21:54:34 +0800

    qsj

---
+++ c++-qsj/p9.41.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<char> str{'q', 's', 'j'};
+    string s(str.cbegin(), str.cend());
+
+    cout << s << endl;
+    
+    return 0;
+}
+
