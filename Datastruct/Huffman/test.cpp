13c7503fa8b351beef12c659d6b654cb8944f9cf
tigersniffsrose5 <1228335053@qq.com>
2019-12-11 22:14:12 +0800

    qsj

---
+++ Datastruct/Huffman/test.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+struct great {
+	bool operator() ( const T &left, const T &right ) {
+        return left > right;
+	}
+};
+
+int main ()
+{
+    priority_queue<int, vector<int>, great<int>> a;
+    priority_queue<int, vector<int>, greater<int>> c;
+
+    for ( int i = 0; i < 5; ++i ) {
+        a.push(i);
+        c.push(i);
+    }
+
+    cout << "a:" << endl;
+    while ( !a.empty() ) {
+        cout << a.top() << endl;
+        a.pop();    
+    }
+
+    cout << "c:" << endl;
+    while ( !c.empty() ) {
+        cout << c.top() << endl;
+        c.pop();
+    }
+
+    return 0;
+}
